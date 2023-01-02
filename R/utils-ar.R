fa97ddb6990394a50ec6907a6e2a4526339cad8f
Josh Bale <joshbale1234@gmail.com>
2023-01-02 11:17:17 +0000

    Added col_mean() function

--- R/utils-ar.R
+++ R/utils-ar.R
@@ -19,3 +19,23 @@ lags <- function(var, n) {
   purrr::map(indices, ~ rlang::quo(dplyr::lag(!!var, !!.x))) %>%
     rlang::set_names(sprintf("lag_%s_%02d", rlang::as_label(var), indices))
 }
+
+
+#' Mean of a list of columns
+#'
+#' This function takes a list of column names and calculates the mean of these columns across rows
+#'
+#'@param cols A character list of column names
+#'@param na.rm Boolean: True (Defalcate) removes missing values from calculation
+#'
+#'
+
+
+col_mean <- function(cols, na.rm = TRUE) {
+  rowMeans(dplyr::across(tidyselect::all_of(cols)), na.rm = na.rm)
+}
+
+
+
+
+
